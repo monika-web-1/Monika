a340bc0abecf8c7a7f523ffb033810e8fb2c85c9
monika-web-1 <monikabansal63828@gmail.com>
2025-07-16 07:18:32 +0000

    Refactor code structure for improved readability and maintainability

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.html", "./src/**/*.{{html,js,jsx,ts,tsx,vue,astro}"],
+  content: ['./**/*.html', './src/**/*.{{html,js,jsx,ts,tsx,vue,astro}'],
   theme: {
-    extend: {
-    },
+    extend: {},
   },
   plugins: [
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/container-queries"),
-    require("prettier-plugin-tailwindcss"),
-    require("@material-tailwind/html/utils/withMT"),
-    require("@tailwindcss/deprecation-warnings"),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+    require('prettier-plugin-tailwindcss'),
+    require('@material-tailwind/html/utils/withMT'),
+    require('@tailwindcss/deprecation-warnings'),
   ],
 };
